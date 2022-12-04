2108c44fa1a7c75d826490adf1e439d36277814e
xovalef <valeria.fuentes1@mail.udp.cl>
2022-12-04 14:10:38 -0300

    prueba db

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from flask import Flask
+import sqlite3
+from flask import g
+
+DATABASSE = './tarea3.db'
+
+def get_db():
+        db = getattr(g,'_database', None)
+        if db is None:
+                db=g._database = sqlite3.connect(DATABASSE)
+        return db
 
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
-def hello():
-        return 'hola'
+@app.route("/")
+def index():
+        cur = get_db().cursor()
+        sql = """SELECT * FROM admin;"""
+        cur.execute(sql)
+        ans = cur.fetchall()
+
+        return ans
---
+++ script.sql
@@ -0,0 +1,49 @@
+CREATE TABLE admin
+(
+    username TEXT PRIMARY KEY,
+    password TEXT
+);
+
+CREATE TABLE company(
+    id INTEGER PRIMARY KEY,
+    company_name TEXT,
+    company_apy_key TEXT);
+
+
+CREATE TABLE location
+(
+    id INTEGER PRIMARY KEY,
+    location_name TEXT,
+    location_country TEXT,
+    location_city TEXT,
+    location_meta TEXT
+);
+
+CREATE TABLE company_location
+(
+    id INTEGER PRIMARY KEY,
+    company_id INTEGER,
+    location_id INTEGER,
+    FOREIGN KEY(company_id) REFERENCES company(id)
+    FOREIGN KEY(location_id) REFERENCES location(id)  
+);
+
+CREATE TABLE sensor
+(
+    id INTEGER PRIMARY KEY,
+    
+    sensor_name TEXT,
+    sensor_category TEXT,
+    sensor_meta TEXT,
+    sensor_api_key TEXT
+    
+);
+
+CREATE TABLE location_sensor
+(
+    id INTEGER PRIMARY KEY,
+    location_id INTEGER,
+    sensor_id INTEGER,
+    FOREIGN KEY(location_id) REFERENCES location(id),
+    FOREIGN KEY(sensor_id) REFERENCES sensor(id)
+);
